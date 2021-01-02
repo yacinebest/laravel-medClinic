f5e021064450d3bb940c18d0a84b174da13f6a7c
yacinebest <yacine.besta@gmail.com>
2021-01-02 11:49:10 +0100

    adding Package laravel/ui=2.* for Auth,moving User to App\Models

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login','Auth\LoginController@showLoginForm')->name('login');
+Route::post('login','Auth\LoginController@login');
+Route::post('logout','Auth\LoginController@logout')->name('logout');
+Route::get('register','Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register','Auth\RegisterController@register');
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+//generating when using Auth::routes() but don't needed instead i just define the needed route
+//GET|HEAD|login                 |login           |Auth\LoginController@showLoginForm
+//POST    |login                 |                |Auth\LoginController@login
+//POST    |logout                |logout          |Auth\LoginController@logout
+//GET|HEAD|password/confirm      |password.confirm|Auth\ConfirmPasswordController@showConfirmForm
+//POST    |password/confirm      |                |Auth\ConfirmPasswordController@confirm
+//POST    |password/email        |password.email  |Auth\ForgotPasswordController@sendResetLinkEmail
+//GET|HEAD|password/reset        |password.request|Auth\ForgotPasswordController@showLinkRequestForm
+//POST    |password/reset        |password.update |Auth\ResetPasswordController@reset
+//GET|HEAD|password/reset/{token}|password.reset  |Auth\ResetPasswordController@showResetForm
+//GET|HEAD|register              |register        |Auth\RegisterController@showRegistrationForm
+//POST    |register              |                |Auth\RegisterController@register
+//Auth::routes();
