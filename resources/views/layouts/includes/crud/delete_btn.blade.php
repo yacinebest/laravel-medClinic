0ad7d093bdf99e94913e5f3b5148292a07d99bc4
yacinebest <yacine.besta@gmail.com>
2021-01-25 07:58:56 +0100

    fixing some thiing missing in template delete.

--- resources/views/layouts/includes/crud/delete_btn.blade.php
+++ resources/views/layouts/includes/crud/delete_btn.blade.php
@@ -1,2 +1,2 @@
 @include('layouts.includes.crud.delete.remove_btn',['id'=>$id,'name_id'=>$name_id])
-@include('layouts.includes.crud.delete.form_btn',['id'=>$id,'name_id'=>$name_id,'route_delete'=>$route_delete])
+@include('layouts.includes.crud.delete.remove_form',['id'=>$id,'name_id'=>$name_id,'route_delete'=>$route_delete])
